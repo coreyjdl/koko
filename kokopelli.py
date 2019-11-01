c306bfdd16c11892eb49d01dc198c3278851972c
Corey <corey.lloyd@fjorgedigital.com>
2019-11-01 10:09:55 -0500

    init

---
+++ kokopelli.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+import requests, datetime, re
+from bs4 import BeautifulSoup
+
+url = "http://www.kokapellideli.com/Lunch.html"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+today = datetime.datetime.today().strftime("%A")
+
+results = []
+for elem in soup.find_all('font'):
+  text = ''.join(char for char in elem.text if ord(char) < 128)
+  if (text):
+    results.append(text)
+
+index_of_today = results.index(today)
+
+with open('koko', 'w') as file:
+  for item in results[index_of_today: index_of_today + 4]:
+    file.write(f"{item}<br />")
